ff2ef7e4d27548a14a30e50d3d5098d347eafe02
Aaron Piotrowski <ap.piotrowski@outlook.com>
2024-03-30 13:11:07 -0400

    Add Nuget EF SQlite and DbContext

--- API/Controllers/TodoController.cs
+++ API/Controllers/TodoController.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TodoController : ControllerBase
 {
-    public class TodoController
+    private readonly ApplicationDbContext _context;
+
+    public TodoController(ApplicationDbContext context)
     {
-        
+        _context = context;
     }
 }

726815b19cf7221418ccfd2b7baeb7fb53b626c0
Aaron Piotrowski <ap.piotrowski@outlook.com>
2024-04-02 13:31:49 -0400

    Add CORS to connect Angular app

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HttpClientModule,
+    MatListModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatExpansionModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  title = 'Task Ticker';
+  todos: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:5048/api/todos').subscribe({
+      next: (data: any) => {
+        this.todos = data;
+        console.log('Data:', data);
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      },
+      complete: () => {
+        console.log('Completed');
+      },
+    });
+  }
 }
